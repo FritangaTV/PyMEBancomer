47001bbf9b3506c470073d86ebca2a935b0d31d0
Sebastian Alonso Rojas <sebasdthamuz@MacBook-Pro-de-Sebastian.local>
2016-10-29 19:06:40 -0500

    termino de separación y uotput.json example

--- app/js/controllers/dashboardCtrl.js
+++ app/js/controllers/dashboardCtrl.js
@@ -1,8 +1,13 @@
-pymeapp.controller('dashboardCtrl', ['$scope', function ($scope) {
-        $scope.datos = {
-        	dineros:123123,
-        	trans:456,
-        	nombre:'sebas'
-        }
+pymeapp.controller('dashboardCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.prediccion = [];
+
+        $http({
+		  method: 'GET',
+		  url: 'json/output.json'
+		}).then(function successCallback(response) {
+			$scope.prediccion = response.data;
+	  	}, function errorCallback(response) {
+
+	  	});
     }
 ]);
