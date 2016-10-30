7916fb93d468630cb38957d77f794cfd5fa6c52f
Sebastian Alonso Rojas <sebasdthamuz@MacBook-Pro-de-Sebastian.local>
2016-10-29 23:00:56 -0500

    imagenes

--- app/js/controllers/tipsCtrl.js
+++ app/js/controllers/tipsCtrl.js
@@ -8,39 +8,46 @@ pymeapp.controller('tipsCtrl', ['$scope', function ($scope) {
         		//Temperatura
         		if(today_weather.mean_temp > 29){ // Día caluroso
         			$scope.tips.push({
-        				desc:"Día caluroso. Contempla tu suministro de alimentos refrescantes"
+        				desc:"Día caluroso. Contempla tu suministro de alimentos refrescantes",
+        				img:"bebida_refrescante.jpeg"
         			});
         		}else if(today_weather.mean_temp < 20){ // Día frio
         			$scope.tips.push({
-        				desc:"Día frío. Contempla tu suministro de alimentos calientitos"
+        				desc:"Día frío. Contempla tu suministro de alimentos calientitos",
+        				img:"bebida_calientita.jpg"
         			});
         		}
         		// Viento
         		if(today_weather.mean_speed > 12){ // Día con viento
         			$scope.tips.push({
-        				desc:"Día con viento. Contempla tu áreas al aire libre"
+        				desc:"Día con viento. Contempla tu áreas al aire libre",
+        				img:"viento.jpeg"
         			});
         		}
         		// Quincena
-        		if(meta.day_num == "01" || meta.day_num == "02"  || meta.day_num == "03"
-        			|| meta.day_num == "15" || meta.day_num == "16" || meta.day_num == "17"){ // Inicio de Quincena
+        		if(meta.day_num == "01" || meta.day_num == "02" || meta.day_num == "03" || meta.day_num == "04"
+        			|| meta.day_num == "15" || meta.day_num == "16" || meta.day_num == "17" || meta.day_num == "18	"){ // Inicio de Quincena
         			$scope.tips.push({
-        				desc:"Inicio de quincena"
+        				desc:"Inicio de quincena",
+        				img:"inicio_quincena.jpg"
         			});
         		}else if(meta.day_num == "12" || meta.day_num == "13" || meta.day_num == "14"
         			|| meta.day_num == "28" || meta.day_num == "29" || meta.day_num == "30" || meta.day_num == "31"){
         			$scope.tips.push({
-        				desc:"Fin de quincena"
+        				desc:"Fin de quincena",
+        				img:"fin_quincena.jpg"
         			});
         		}
         		// Semana
-        		if(meta.weekday == "Saturday" || meta.weekday == "Sunday"){
+        		if(meta.weekday == "Saturday" || meta.weekday == "Sunday"  || meta.weekday == "Friday"){
         			$scope.tips.push({
-        				desc:"Fin de semana"
+        				desc:"Fin de semana (o viernes)",
+        				img:"fin_semana.jpg"
         			});
         		}else{
         			$scope.tips.push({
-        				desc:"Entre semana"
+        				desc:"Entre semana",
+        				img:"entre_semana.jpg"
         			});
         		}
         		
