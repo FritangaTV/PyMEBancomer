b8037d91b8ba0625d6c648bd4e0ef382c6568ffd
Sebastian Alonso Rojas <sebasdthamuz@MacBook-Pro-de-Sebastian.local>
2016-10-29 21:34:32 -0500

    output

--- app/js/controllers/tipsCtrl.js
+++ app/js/controllers/tipsCtrl.js
@@ -2,7 +2,6 @@ pymeapp.controller('tipsCtrl', ['$scope', function ($scope) {
         $scope.tips = [];
         $scope.$watch('prediccion', function() {
         	if(typeof $scope.predicciones[0] !== 'undefined'){
-        		//Por clima
         		
         	}
     	});
