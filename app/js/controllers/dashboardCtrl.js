f1398f213cfc40b12ec117e7ff80bd957517cda2
Psyrax <psyrax@opiumgarden.org>
2016-10-30 10:25:49 -0600

    stranger things

--- app/js/controllers/dashboardCtrl.js
+++ app/js/controllers/dashboardCtrl.js
@@ -26,15 +26,52 @@ pymeapp.controller('dashboardCtrl', ['$scope', '$http', function ($scope, $http)
         	format: 'YYYY-MM-DD'
         });
 
+        $scope.dataSamples = [
+            {
+                "url": "/json/samples/sample1/output.json",
+                "client": 6996,
+                "client_name": "Fonda Juanita",
+                "initDate": "2015-12-01",
+                "endDate" : "2016-03-01",
+                "logo" : "/images/logos/juanita.jpg"
+            },
+            {
+                "url": "/json/samples/sample1/buena.json",
+                "client": 3328,
+                "client_name": "Buena onda",
+                "initDate": "2015-12-01",
+                "endDate" : "2016-03-01",
+                "logo" : "/images/logos/goodVibes.jpg"
+            },
+            {
+                "url": "/json/samples/sample1/promedio.json",
+                "client": 3600,
+                "client_name": "El regular",
+                "initDate": "2015-12-01",
+                "endDate" : "2016-06-10",
+                "logo" : "/images/logos/regular.jpg"
+            }
+        ];
+
+        $scope.selectedSample = $scope.dataSamples[0];
+        $scope.$watchCollection('selectedSample', function() {
+            console.log($scope.selectedSample);
+            getPredicciones($scope.selectedSample.url);
+        });
+
+
         $scope.predicciones = [];
 
-        $http({
-		  method: 'GET',
-		  url: '/json/samples/sample1/output.json'
-		}).then(function successCallback(response) {
-			$scope.predicciones = response.data;
-	  	}, function errorCallback(response) {
 
-	  	});
+        function getPredicciones(urlGet){
+            $http({
+              method: 'GET',
+              url: urlGet
+            }).then(function successCallback(response) {
+                $scope.predicciones = response.data;
+            }, function errorCallback(response) {
+
+            });
+        };
     }
 ]);
